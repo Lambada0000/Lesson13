f9f4548f163ea4e3ac042324f920b845c66f470f
георгий савинцев <igsgo4645@gmail.com>
2024-04-10 21:36:57 +0400

    add corrections

--- classes.py
+++ classes.py
@@ -1,24 +1,34 @@
+
 class Category:
     title: str
-    descritions: str
+    descriptions: str
     products: list
+    all_categories = []
+    total_category: int
+    total_unique_product: int
 
-    def __init__(self, title, descriptoins, products):
+    def __init__(self, title, descriptions, products):
         self.title = title
-        self.descritions = descriptoins
+        self.descriptions = descriptions
         self.products = products
-        self.total_category = len(products)
-        self.total_unique_product = 10
+        self.all_categories.append(self)
+        self.total_category = 0
+        self.total_unique_product = 0
+
+        for category in self.all_categories:
+            self.total_category += 1
+            for _ in category.products:
+                self.total_unique_product += 1
 
 
 class Product:
     title: str
-    descritions: str
+    descriptions: str
     pay: float
     quantity: int
 
     def __init__(self, title, descriptions, pay, quantity):
         self.title = title
-        self.descritions = descriptions
+        self.descriptions = descriptions
         self.pay = float(pay)
         self.quantity = quantity
--- test.py
+++ test.py
@@ -3,25 +3,25 @@ import pytest
 
 
 @pytest.fixture
-def Category_Ponshiki():
-    return Category('Пончики', 'Очень вкусные, но вредные', ['Клубничные', 'Ванильные', 'Шоколадные'])
+def Category_electronics():
+    return Category('Электроника',
+                    'Компоненты, необходимые для работоспособности разных электрических приборов',
+                    ['Батарея', 'Лампочка', 'Микросхема'])
 
-
-def test_init1(Category_Ponshiki):
-    assert Category_Ponshiki.title == 'Пончики'
-    assert Category_Ponshiki.descritions == 'Очень вкусные, но вредные'
-    assert Category_Ponshiki.products == ['Клубничные', 'Ванильные', 'Шоколадные']
-    assert Category_Ponshiki.total_category == 3
-    assert Category_Ponshiki.total_unique_product == 10
-
+def test_init1(Category_electronics):
+    assert Category_electronics.title == 'Электроника'
+    assert Category_electronics.descriptions == 'Компоненты, необходимые для работоспособности разных электрических приборов'
+    assert Category_electronics.products == ['Батарея', 'Лампочка', 'Микросхема']
+    assert Category_electronics.total_category == 1
+    assert Category_electronics.total_unique_product == 3
 
 @pytest.fixture
-def Product_Choco():
-    return Product('Шоколадные пончики', 'Шоколадная начинка!', 89.99, 57)
+def Product_Battery():
+    return Product('Батарея', 'Автономный источник постоянного тока',100, 200)
 
 
-def test_init2(Product_Choco):
-    assert Product_Choco.title == 'Шоколадные пончики'
-    assert Product_Choco.descritions == 'Шоколадная начинка!'
-    assert Product_Choco.pay == 89.99
-    assert Product_Choco.quantity == 57
+def test_init2(Product_Battery):
+    assert Product_Battery.title == 'Батарея'
+    assert Product_Battery.descriptions == 'Автономный источник постоянного тока'
+    assert Product_Battery.pay == 100
+    assert Product_Battery.quantity == 200
